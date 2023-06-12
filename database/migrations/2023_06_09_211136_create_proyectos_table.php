1a671b149a25cda4d7deb79ffa1d8b2ba3923a18
030366 <blackangelset@gmail.com>
2023-06-11 18:17:52 -0600

    primera subida

---
+++ database/migrations/2023_06_09_211136_create_proyectos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->id();
+            $table->string('NombreProyecto',100);
+            $table->string('FuenteFondos',100);
+            $table->float('MontoPlanificado');
+            $table->float('MontoPatrocinado');
+            $table->float('MontoFondosPropios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyectos');
+    }
+};
